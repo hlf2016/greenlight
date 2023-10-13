5c8c615b086d94870697375a532bd2f516d8b8ee
Pannio <1821299140@qq.com>
2023-10-14 00:55:53 +0800

    自定义全局 NotFound 和MethodNotAllowed 错误处理器

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -8,6 +8,11 @@ import (
 func (app *application) routes() *httprouter.Router {
 	router := httprouter.New()
 
+	// 使用 http.HandlerFunc() 适配器将 notFoundResponse() 辅助程序转换为 http.Handler 程序，然后将其设置为 404 Not Found 响应的自定义错误处理程序。
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+	// 同样，将 methodNotAllowedResponse() 转换为 http.Handler，并将其设置为 405 Method Not Allowed 响应的自定义错误处理程序。
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
+
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
