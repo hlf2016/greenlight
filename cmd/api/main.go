7f09a78e20369307db77a743a61b30879369c17d
Pannio <1821299140@qq.com>
2023-10-17 14:26:23 +0800

    调整数据库配置 提升性能

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -18,7 +18,10 @@ type config struct {
 	port int
 	env  string
 	db   struct {
-		dsn string
+		dsn          string
+		maxOpenConns int
+		maxIdleConns int
+		maxIdleTime  string
 	}
 }
 
@@ -32,8 +35,14 @@ func main() {
 	flag.IntVar(&cfg.port, "port", 4000, "Api server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
 
-	//  pq: SSL is not enabled on the server 需要在 dsn 禁用 ssl
-	flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://greenlight:25804769@localhost/greenlight?sslmode=disable", "PostgreSQL DSN")
+	// 若报错 pq: SSL is not enabled on the server 需要在 dsn 禁用 ssl
+	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "PostgreSQL DSN")
+
+	// 将连接池设置从命令行标志读入配置结构。注意到我们使用的默认值了吗？
+	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
+	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
+	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max connection idle time")
+
 	flag.Parse()
 
 	// 初始化一个新的日志记录器，将信息写入标准输出流，并以当前日期和时间为前缀。
@@ -72,6 +81,19 @@ func main() {
 func openDB(cfg config) (*sql.DB, error) {
 	// 使用 sql.Open()，使用配置结构中的 DSN 创建一个空连接池。
 	db, err := sql.Open("postgres", cfg.db.dsn)
+
+	// 设置池中打开（使用中 + 空闲）连接的最大数量。请注意，如果传递的值小于或等于 0，则表示没有限制。
+	db.SetMaxOpenConns(cfg.db.maxOpenConns)
+	// 设置池中空闲连接的最大数量。同样，如果传递的值小于或等于 0，则表示没有限制。
+	db.SetMaxIdleConns(cfg.db.maxIdleConns)
+	// 使用 time.ParseDuration() 函数将空闲超时持续时间字符串转换为 time.Duration 类型。
+	duration, err := time.ParseDuration(cfg.db.maxIdleTime)
+	if err != nil {
+		return nil, err
+	}
+	// 设置最大空闲超时。
+	db.SetConnMaxIdleTime(duration)
+
 	if err != nil {
 		return nil, err
 	}
