1d875a553ef66a457e987bfec74c54f3265e9234
Pannio <1821299140@qq.com>
2023-10-26 00:12:11 +0800

    基于 IP 的请求速率限制

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"golang.org/x/time/rate"
+	"net"
 	"net/http"
+	"sync"
 )
 
 func (app *application) recoverPanic(next http.Handler) http.Handler {
@@ -26,17 +28,50 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 }
 
 func (app *application) rateLimit(next http.Handler) http.Handler {
+	// 声明一个 mutex 和一个 map，用于保存客户端的 IP 地址和速率限制器。
+	var (
+		mu      sync.Mutex
+		clients = make(map[string]*rate.Limiter)
+	)
 	// 初始化一个新的速率限制器，允许平均每秒 2 个请求，单个 "突发 "最多 4 个请求。
-	limiter := rate.NewLimiter(2, 4)
+	// limiter := rate.NewLimiter(2, 4)
+
 	// 我们返回的函数是一个闭包，它 "关闭 "了限制器变量。
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// 从请求中提取客户端的 IP 地址。
+		ip, _, err := net.SplitHostPort(r.RemoteAddr)
+		if err != nil {
+			app.serverErrorResponse(w, r, err)
+			return
+		}
+
+		// 上互斥锁，以防止这段代码被同时执行。
+		mu.Lock()
+
+		// 检查 IP 地址是否已存在于地图中。如果不存在，则初始化一个新的速率限制器，并将 IP 地址和限制器添加到映射中。
+		if _, found := clients[ip]; !found {
+			clients[ip] = rate.NewLimiter(2, 4)
+		}
+
 		// 调用 limiter.Allow() 查看请求是否允许，如果不允许，则调用 rateLimitExceededResponse() 辅助程序返回 429 太多请求响应（我们稍后将创建该辅助程序）。
 		// 每当我们调用速率限制器上的 Allow() 方法时，就会从邮筒中消耗一个令牌。如果桶中没有剩余的令牌，Allow() 方法将返回 false，并触发向客户端发送 429 太多请求的响应。
 		// 还需要注意的是，Allow() 方法后面的代码受互斥保护，可以安全并发使用。
-		if !limiter.Allow() {
+		//if !limiter.Allow() {
+		//	app.rateLimitExceededResponse(w, r)
+		//	return
+		//}
+
+		// 调用当前 IP 地址的速率限制器上的 Allow() 方法。如果请求不被允许，则解锁互斥器 并发送 429 太多请求响应，就像之前一样。
+		if !clients[ip].Allow() {
+			mu.Unlock()
 			app.rateLimitExceededResponse(w, r)
 			return
 		}
+
+		// 最重要的是，在调用链中的下一个处理程序之前，要先解开互斥锁。
+		// 请注意，我们不能使用 defer 来解锁互斥，因为这意味着在该中间件下游的所有处理程序都返回之前，互斥不会被解锁。
+		mu.Unlock()
+
 		next.ServeHTTP(w, r)
 	})
 }
