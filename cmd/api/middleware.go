a9bf91309f0ff5e16c4c2091a2e46951aa389580
Pannio <1821299140@qq.com>
2023-10-26 14:41:14 +0800

    淘汰3分钟不用的limiter 节省内存

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -6,6 +6,7 @@ import (
 	"net"
 	"net/http"
 	"sync"
+	"time"
 )
 
 func (app *application) recoverPanic(next http.Handler) http.Handler {
@@ -28,11 +29,33 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 }
 
 func (app *application) rateLimit(next http.Handler) http.Handler {
+	// 定义一个client结构，用于保存每个客户端的速率限制器和最后查看时间。
+	type client struct {
+		limiter  *rate.Limiter
+		lastSeen time.Time
+	}
 	// 声明一个 mutex 和一个 map，用于保存客户端的 IP 地址和速率限制器。
 	var (
 		mu      sync.Mutex
-		clients = make(map[string]*rate.Limiter)
+		clients = make(map[string]*client)
 	)
+
+	// 启动后台程序，每分钟从客户端地图中删除一次旧条目。
+	go func() {
+		for {
+			time.Sleep(time.Minute)
+			// 开启互斥锁，防止在清理过程中进行任何速率限制器检查。
+			mu.Lock()
+			// 循环浏览所有客户端。如果在过去三分钟内没有看到它们，就从地图上删除相应的条目。
+			for ip, client := range clients {
+				if time.Since(client.lastSeen) > 3*time.Minute {
+					delete(clients, ip)
+				}
+			}
+			// 重要的是，清理完成后要解锁互斥。
+			mu.Unlock()
+		}
+	}()
 	// 初始化一个新的速率限制器，允许平均每秒 2 个请求，单个 "突发 "最多 4 个请求。
 	// limiter := rate.NewLimiter(2, 4)
 
@@ -50,8 +73,9 @@ func (app *application) rateLimit(next http.Handler) http.Handler {
 
 		// 检查 IP 地址是否已存在于地图中。如果不存在，则初始化一个新的速率限制器，并将 IP 地址和限制器添加到映射中。
 		if _, found := clients[ip]; !found {
-			clients[ip] = rate.NewLimiter(2, 4)
+			clients[ip] = &client{limiter: rate.NewLimiter(2, 4)}
 		}
+		clients[ip].lastSeen = time.Now()
 
 		// 调用 limiter.Allow() 查看请求是否允许，如果不允许，则调用 rateLimitExceededResponse() 辅助程序返回 429 太多请求响应（我们稍后将创建该辅助程序）。
 		// 每当我们调用速率限制器上的 Allow() 方法时，就会从邮筒中消耗一个令牌。如果桶中没有剩余的令牌，Allow() 方法将返回 false，并触发向客户端发送 429 太多请求的响应。
@@ -62,7 +86,7 @@ func (app *application) rateLimit(next http.Handler) http.Handler {
 		//}
 
 		// 调用当前 IP 地址的速率限制器上的 Allow() 方法。如果请求不被允许，则解锁互斥器 并发送 429 太多请求响应，就像之前一样。
-		if !clients[ip].Allow() {
+		if !clients[ip].limiter.Allow() {
 			mu.Unlock()
 			app.rateLimitExceededResponse(w, r)
 			return
